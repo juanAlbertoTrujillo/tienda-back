1c8647a63d0913502c3e8fbf7893d94de50a2c30
juanAlbertoTrujillo <juan.tru.nav@gmail.com>
2019-12-26 17:35:38 -0800

    make it better

--- dist/controller/articulo.controller.js
+++ dist/controller/articulo.controller.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Articulo_1 = __importDefault(require("../models/Articulo"));
 const Query_1 = require("../models/Query");
 async function obtenerArticulos(req, res) {
-    console.log(Query_1.buscarMasDeUno(Articulo_1.default));
-    return res.status(200).json(Query_1.buscarMasDeUno(Articulo_1.default));
+    console.log(await Query_1.buscarMasDeUno(Articulo_1.default));
+    return res.status(200).json(await Query_1.buscarMasDeUno(Articulo_1.default));
 }
 exports.obtenerArticulos = obtenerArticulos;
 async function agregarArticulo(req, res) {
--- src/controller/articulo.controller.ts
+++ src/controller/articulo.controller.ts
@@ -4,9 +4,9 @@ import { buscarMasDeUno, guardar } from '../models/Query'
 
 export async function obtenerArticulos(req: Request, res: Response): Promise<Response> {
 
-    console.log(buscarMasDeUno(Articulo))
+    console.log(await buscarMasDeUno(Articulo))
 
-    return res.status(200).json(buscarMasDeUno(Articulo));
+    return res.status(200).json(await buscarMasDeUno(Articulo));
    
 }
 
